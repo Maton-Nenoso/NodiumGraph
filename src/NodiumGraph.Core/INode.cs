804578c41d3c8671d2d6685dec6f72da5ed8a47b
Stefan Maton <maton@nenoso.be>
2026-04-08 18:50:39 +0200

    Add NodiumGraph.Core class library with INode interface

---
+++ src/NodiumGraph.Core/INode.cs
@@ -0,0 +1,13 @@
+namespace NodiumGraph.Core;
+
+/// <summary>
+/// Represents a node in the graph. Implemented by consumer types.
+/// </summary>
+public interface INode
+{
+    Guid Id { get; }
+    double X { get; set; }
+    double Y { get; set; }
+    double Width { get; }
+    double Height { get; }
+}
