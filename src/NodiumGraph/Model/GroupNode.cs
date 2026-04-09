b58b6c2653a3a99c28e15bac1818cbb97a5b8c30
Stefan Maton <maton@nenoso.be>
2026-04-09 12:35:08 +0200

    Propagate GroupNode position changes to children

--- src/NodiumGraph/Model/GroupNode.cs
+++ src/NodiumGraph/Model/GroupNode.cs
@@ -1,16 +1,40 @@
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace NodiumGraph.Model;
 
 public class GroupNode : Node
 {
     private readonly ObservableCollection<Node> _children = new();
+    private double _lastX;
+    private double _lastY;
 
     public ReadOnlyObservableCollection<Node> Children { get; }
 
     public GroupNode()
     {
         Children = new(_children);
+        _lastX = X;
+        _lastY = Y;
+        PropertyChanged += OnSelfPropertyChanged;
+    }
+
+    private void OnSelfPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(X))
+        {
+            var delta = X - _lastX;
+            _lastX = X;
+            foreach (var child in _children)
+                child.X += delta;
+        }
+        else if (e.PropertyName == nameof(Y))
+        {
+            var delta = Y - _lastY;
+            _lastY = Y;
+            foreach (var child in _children)
+                child.Y += delta;
+        }
     }
 
     public void AddChild(Node node)
--- tests/NodiumGraph.Tests/GroupNodeTests.cs
+++ tests/NodiumGraph.Tests/GroupNodeTests.cs
@@ -54,4 +54,36 @@ public class GroupNodeTests
         var group = new GroupNode();
         Assert.Throws<ArgumentNullException>(() => group.AddChild(null!));
     }
+
+    [Fact]
+    public void Moving_group_moves_children()
+    {
+        var group = new GroupNode { X = 100, Y = 100 };
+        var child = new Node { X = 150, Y = 150 };
+        group.AddChild(child);
+
+        group.X = 200;
+        Assert.Equal(250, child.X); // moved by delta 100
+
+        group.Y = 300;
+        Assert.Equal(350, child.Y); // moved by delta 200
+    }
+
+    [Fact]
+    public void Moving_group_moves_multiple_children()
+    {
+        var group = new GroupNode { X = 0, Y = 0 };
+        var c1 = new Node { X = 10, Y = 20 };
+        var c2 = new Node { X = 30, Y = 40 };
+        group.AddChild(c1);
+        group.AddChild(c2);
+
+        group.X = 50;
+        group.Y = 60;
+
+        Assert.Equal(60, c1.X);
+        Assert.Equal(80, c1.Y);
+        Assert.Equal(80, c2.X);
+        Assert.Equal(100, c2.Y);
+    }
 }
